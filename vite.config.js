dfd6baca086a778b6d13e9b7beb990841f558a4f
tokoshika <103026403+tokoshika@users.noreply.github.com>
2023-01-31 14:25:50 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV = 'production' ? '/micro-test/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/micro-test/' : './',
 
   plugins: [vue()],
   resolve: {
